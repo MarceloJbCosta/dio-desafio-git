256a370f34d771a1050670a10d40385d074a6462
marcelojbcosta <marcelomogueco@gmail.com>
2022-01-07 16:34:50 -0300

    Alterando projeto aula 20

--- BootCamp DIO/20 -Criando um APP simples de cadastro de séries em .NET/DIO.Series/Classes/Serie.cs
+++ BootCamp DIO/20 -Criando um APP simples de cadastro de séries em .NET/DIO.Series/Classes/Serie.cs
@@ -8,6 +8,8 @@ namespace DIO.Series
 
         private string Descricao {get; set;}
         private  int Ano { get; set;}
+
+        private bool Excluido {get; set;}
         
         //metodos
 
@@ -17,6 +19,7 @@ namespace DIO.Series
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -26,6 +29,7 @@ namespace DIO.Series
             retorno += "Titulo: " + this.Titulo + Environment.NewLine;
             retorno += "Descricao: " + this.Descricao + Environment.NewLine;
             retorno += "Ano de Inicio: " + this.Ano;
+            retorno += "Excluido: " + this.Excluido;
             return retorno;
         }
 
@@ -38,6 +42,11 @@ namespace DIO.Series
         public int retornaId()
         {
             return this.Id;
+        }
+
+        public void Exclui()
+        {
+            this.Excluido = true;
         }
      
         
